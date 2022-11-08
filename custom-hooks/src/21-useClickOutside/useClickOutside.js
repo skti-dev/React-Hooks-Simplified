28ff4c226471122686798ddda6da3481f225e6bb
Augusto Seabra <38216218+skti-dev@users.noreply.github.com>
2022-11-07 21:50:03 -0300

    Delete custom hooks

--- custom-hooks/src/21-useClickOutside/useClickOutside.js
+++
@@ -1,8 +0,0 @@
-import useEventListener from '../13-useEventListener/useEventListener'
-
-export default function useClickOutside(ref, callback) {
-  useEventListener('click', e => {
-    if(ref.current == null || ref.current.contains(e.target)) return
-    callback(e)
-  }, document)
-}
